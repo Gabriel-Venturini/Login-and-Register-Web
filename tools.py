4ecaf24bbc7c912199592dc7233f1444fc8c1b55
Gabriel Venturini <dev.venturini@gmail.com>
2023-11-08 19:33:51 -0300

    backend: tools|json_file creation

---
+++ tools.py
@@ -0,0 +1,45 @@
+import json
+
+def read_json():
+    ''' Opens the JSON file to use it whenever needed '''
+    with open('users.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def write_json(new_user):
+    ''' Gets the actual JSON file then changes
+    it content '''
+    data_file = read_json()
+    data_file.update(new_user)
+    with open('users.json', 'w') as file:
+        json.dump(data_file, file, indent=4)
+
+
+def register(login, psswd, email):
+    ''' Gets 3 args then create a formatted var for them '''
+    new_user = {
+        login: [
+            {
+                "email": email,
+                "password": psswd
+            }
+        ]
+    }
+    write_json(new_user)
+
+
+def login(login, psswd):
+    ''' Opens JSON file then search for the user
+    in the list using the name as an index '''
+    data = read_json()
+    for person in data[login.lower()]:
+        if psswd == person['password']:
+            return 1
+        else:
+            return 0
+
+
+# new_user = register()
+# login() 
+# I used this section to test the code
