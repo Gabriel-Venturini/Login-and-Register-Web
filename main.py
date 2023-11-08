bc7a2ed6439d7f40eba70c9a4160992768cbf536
Gabriel Venturini <dev.venturini@gmail.com>
2023-11-08 19:35:05 -0300

    backend: Login implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import csv
+import json
+import tools
 
 
 tab1, tab2 = st.tabs(['Login', 'Register'])
@@ -14,21 +15,26 @@ pass_requirements = '''
 
 with tab1:
     with st.form("login_form"):
-        st.text_input('E-mail', key='email')
-        st.text_input('Password', key='psswd', type='password')
+        user_login = st.text_input('Username')
+        user_psswd = st.text_input('Password', type='password')
         submit = st.form_submit_button('Sign in', use_container_width=True)
-        
+        if submit:
+            response = tools.login(login=user_login, psswd=user_psswd)
+            if response == 1:
+                st.text('ENTROU')
+            else:
+                st.text('NAO ENTROU')
 with tab2:
     with st.form("sign_up"):
         st.text_input('E-mail', max_chars=40, key='get_email')
-        user_name = st.text_input('Username', type='default', 
+        name = st.text_input('Username', type='default', 
             max_chars=20, help="Use . instead of spaces", key='get_user')
-        user_psswd = st.text_input('Password', type='password', 
+        psswd = st.text_input('Password', type='password', 
             help='' ,key='get_psswd')
-        user_psswd_confirm = st.text_input('Confirm your password', type='password',
+        psswd_confirm = st.text_input('Confirm your password', type='password',
                                         key='get_confirm_psswd')
         pass_expander = st.expander('Password requirements:')
         pass_expander.write(pass_requirements)
         submit = st.form_submit_button('Sign up', use_container_width=True)
 
-# st.session_state
+st.session_state
